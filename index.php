6c199b256b389e3a1162673af6f6d60cff045753
Renata <125055208@upq.edu.mx>
2026-09-18 17:24:09 -0600

    Agregué estilos básicos con colores, márgenes y rellenos en index.php

--- index.php
+++ index.php
@@ -11,7 +11,25 @@
     h1{
         color: #b65df1;;
     }
-</style>
+.seccion {
+    background-color: lightgreen;
+    padding: 20px 40px;
+    margin:20 px;
+    border: 10px solid black;
+}
+  .seccion2 {
+    background-color: lightpink;
+    padding: 20px 40px;
+    margin-top: 120px;
+    margin-right: 30px;
+    margin-bottom: 40px;
+    margin-left: 50px;
+    padding-top: 20px;
+    padding-right: 40px;
+    padding-bottom: 20px;
+    padding-left: 40px;
+}
+</style> 
 </head>
 <body>
     <h1>Hola Mundo</h1>
@@ -20,7 +38,14 @@
     <h4>Hola Mundo</h4>
     <h5>Hola Mundo</h5>
     <h6>Hola Mundo</h6>
-
+<div class="seccion">
+  <h2>Título del párrafo</h2>
+ <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde enim repellat optio perspiciatis veritatis voluptates animi dolorum inventore beatae ut quas recusandae officiis, maiores qui repudiandae rerum blanditiis accusamus quae.</p>
+</div>
+<div class="seccion2">
+  <h2>Título del párrafo</h2>
+ <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde enim repellat optio perspiciatis veritatis voluptates animi dolorum inventore beatae ut quas recusandae officiis, maiores qui repudiandae rerum blanditiis accusamus quae.</p>
+</div>
 
 
 </body>
